7932f679b624813d4089349f15c15b71a57df89c
aparmer1 <aparmer1@my.westga.edu>
2015-03-22 13:31:54 -0700

    Added validation to model for uniqueness.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ActiveRecord::Base
   searchkick autocomplete: ['name']
+  
+  validates :name, :uniqueness => {:scope => [:address1, :zip], message: 'The restaurant at this address has already been created.'}
+
 end
